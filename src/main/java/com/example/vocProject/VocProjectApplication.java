89debd7e6ad4e0c6bf8ea50d3d201cf2221755a9
Dote10 <zoqol@daum.net>
2021-12-15 14:17:45 +0900

    no message

---
+++ src/main/java/com/example/vocProject/VocProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vocProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vocProject/VocProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vocProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
